1c48f23b46e81db6c252a5214c4209a635421b9d
liceth1006 <olmos.1006@gmail.com>
2024-07-16 12:45:31 -0500

    cards

--- src/Components/Cards/Card/index.jsx
+++ src/Components/Cards/Card/index.jsx
@@ -1,8 +1,18 @@
-const Card = ({ titulo, categoria, imagen, link, descripcion }) => {
+import styled from "styled-components";
+
+const ContainerCard = styled.div`
+width: 430px;
+margin-bottom: 24px;
+position: relative;
+justify-content: space-between;
+gap: 24px;
+`
+
+const Card = ({ titulo,link, descripcion }) => {
   return (
     <>
-      <div>
-        <button>{categoria}</button>
+      <ContainerCard>
+        
         <iframe
           width="560"
           height="315"
@@ -13,7 +23,7 @@ const Card = ({ titulo, categoria, imagen, link, descripcion }) => {
         ></iframe>
         <h2>{titulo}</h2>
         <p>{descripcion}</p>
-      </div>
+      </ContainerCard>
     </>
   );
 };
--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -1,6 +1,24 @@
 import { useEffect, useState } from "react";
 import { conexion } from "../../data/conexion.js";
 import Card from "./Card/index.jsx";
+import styled from "styled-components";
+
+const ContainerCard = styled.div`
+  width: 100%;
+  background-color: #D9F7E9;
+  padding: 32px;
+  box-sizing: border-box;
+  text-align: center;
+  font-size: 32px;
+  position: relative;
+`
+
+
+
+
+
+
+
 
 const Cards = () => {
   const [video, setVideo] = useState([]);
@@ -16,13 +34,13 @@ const Cards = () => {
   }, []);
 
   return (
-    <>{
+    <ContainerCard>
+      {
       video.map((item)=>{
         return <Card key={item.id} {...item}></Card>
       })
     }
-      <h2>ChARDS</h2>
-    </>
+    </ContainerCard>
   );
 };
 
